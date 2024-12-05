5ffefc4ee4c3c4b1419fb3e70dd2455f4ce50f12
Konstantinos Papoulias <kostaspapoulias@gmail.com>
2024-12-05 18:39:46 +0200

    updates

--- main-phase02/voting.h
+++ main-phase02/voting.h
@@ -15,134 +15,19 @@
 #ifndef VOTING_H
 #define VOTING_H
 
-#include <limits.h>
-#include <stdbool.h>
-#include <stdio.h>
-
-/*** Districts Array  ***/
-
-#define PARTIES_SZ 5
-typedef struct District District;
-struct District {
-    int did;
-    int seats;
-    int blanks;
-    int invalids;
-    int partyVotes[PARTIES_SZ];
-};
-#define DISTRICTS_SZ 56
-typedef struct DistrictsArray DistrictsArray;
-struct DistrictsArray {
-    District data[DISTRICTS_SZ];
-};
-
-/*** Voters Tree, Stations Hashtable  ***/
-
-typedef struct Voter Voter;
-struct Voter {
-    int vid;
-    bool voted;
-    Voter* parent;
-    Voter* lc;
-    Voter* rc;
-};
-typedef struct VotersTree VotersTree;
-struct VotersTree {
-    Voter* root;
-};
-typedef struct Station Station;
-struct Station {
-    int sid;
-    int did;
-    int registered;
-    VotersTree voters;
-    Station* next;
-};
-typedef struct StationsList StationsList;
-struct StationsList {
-    Station* begin;
-};
-typedef struct StationsHashTable StationsHashTable;
-struct StationsHashTable {
-    StationsList* data;
-    int capacity;
-};
-extern int ParsedMaxStationsCount;
-extern int ParsedMaxSid;
-
-/*** Candidates Binary Search Tree, Parties Array  ***/
-
-typedef struct Candidate Candidate;
-struct Candidate {
-    int cid;
-    int pid;
-    int did;
-    int votes;
-    bool isElected;
-    Candidate* lc;
-    Candidate* rc;
-};
-typedef struct CandidatesBST CandidatesBST;
-struct CandidatesBST {
-    Candidate* root;
-};
-typedef struct Party Party;
-struct Party {
-    int pid;
-    int electedCount;
-    CandidatesBST candidates;
-};
-typedef struct PartiesArray PartiesArray;
-struct PartiesArray {
-    Party data[PARTIES_SZ];
-};
-
-/*** Parliament List (sorted descending)  ***/
-
-typedef struct ElectedCandidate ElectedCandidate;
-struct ElectedCandidate {
-    int cid;
-    int pid;
-    int did;
-    ElectedCandidate* next;
-};
-typedef struct ParliamentList ParliamentList;
-struct ParliamentList {
-    ElectedCandidate* begin;
-};
-
-/*** Bonus Candidates Binary Search Tree with Sentinel  ***/
-
-typedef struct BonusCandidatesBST BonusCandidatesBST;
-struct BonusCandidatesBST {
-    Candidate* root;
-    Candidate* sentinel;
-};
-
-// Enable in Makefile
-#ifdef DEBUG_PRINTS_ENABLED
-#define DebugPrint(...) printf(__VA_ARGS__);
-#else
-#define DebugPrint(...)
-#endif
-
-void EventAnnounceElections();
+void EventAnnounceElections(int maxStationsCount, int maxSid);
 void EventCreateDistrict(int did, int seats);
 void EventCreateStation(int sid, int did);
-void EventRegisterCandidate(int cid, int did, int pid);
 void EventRegisterVoter(int vid, int sid);
+void EventRegisterCandidate(int cid, int pid, int did);
 void EventVote(int vid, int sid, int cid, int pid);
 void EventCountVotes(int did);
-void EventFormGovernment();
-void EventFormParliament();
+void EventFormParliament(void);
 void EventPrintDistrict(int did);
 void EventPrintStation(int sid);
 void EventPrintParty(int pid);
-void EventPrintParliament();
-void EventBonusAnnounceElections();
+void EventPrintParliament(void);
 void EventBonusUnregisterVoter(int vid, int sid);
-void EventBonusRegisterCandidate(int cid, int did, int pid);
-void EventBonusSplitCandidates();
-void EventBonusFreeMemory();
+void EventBonusFreeMemory(void);
 
 #endif
